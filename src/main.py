6359dd91970cbf6350f6ae3f866a1b32a0d3e33e
Paradis Keza <p.keza1@alustudent.com>
2026-09-11 12:37:47 +0200

    Corrected indention errors

--- src/main.py
+++ src/main.py
@@ -8,11 +8,11 @@ with open("input/raw-text.txt", "r") as file:
     emails=re.findall(pattern,text)
     alu_emails=[]
     print("Emails are:", emails)
-     for email in emails:
+    for email in emails:
        if re.match( r"^[a-zA-Z0-9._%+-]+@(alueducation\.com|alumni\.alueducation\.com|si\.alueducation\.com)$",email):
            alu_emails.append(email)
     print("ALU emails are:", alu_emails)
-     card_pattern=r"\b(?:\d{4}[- ]?){3}\d{4}\b"
+    card_pattern=r"\b(?:\d{4}[- ]?){3}\d{4}\b"
     card=re.findall(card_pattern,text)
     m_card=[]
     for c in card:
@@ -23,7 +23,7 @@ with open("input/raw-text.txt", "r") as file:
     t_pattern=r"(?:\+250[\s-]?|0)7\d{2}[\s-]?\d{3}[\s-]?\d{3}"
     tel=re.findall(t_pattern, text)
     print("The phone numbers are:", tel)
-     u_pattern=r"https?://[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}(?:/[^\s]*)?"
+    u_pattern=r"https?://[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}(?:/[^\s]*)?"
     url=re.findall(u_pattern, text)
     print("The URLs are:", url)
     results={
