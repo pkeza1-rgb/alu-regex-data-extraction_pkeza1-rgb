643e288ef5c74fd77fffb2dab5d5afd087521a77
Paradis Keza <p.keza1@alustudent.com>
2026-09-11 12:23:35 +0200

    Added alu emails

--- src/main.py
+++ src/main.py
@@ -5,5 +5,9 @@ with open("input/raw-text.txt", "r") as file:
     text= file.read()
     pattern=r"[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}"
     emails=re.findall(pattern,text)
-    print(emails)
-    
+    alu_emails=[]
+    print("Emails are:", emails)
+     for email in emails:
+       if re.match( r"^[a-zA-Z0-9._%+-]+@(alueducation\.com|alumni\.alueducation\.com|si\.alueducation\.com)$",email):
+           alu_emails.append(email)
+    print("ALU emails are:", alu_emails)
