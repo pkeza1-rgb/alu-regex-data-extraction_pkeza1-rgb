7ca44a7f8c81278a4b7acce5d198f92bfe6052de
Paradis Keza <p.keza1@alustudent.com>
2026-09-11 12:24:44 +0200

    Added credit card numbers

--- src/main.py
+++ src/main.py
@@ -11,3 +11,11 @@ with open("input/raw-text.txt", "r") as file:
        if re.match( r"^[a-zA-Z0-9._%+-]+@(alueducation\.com|alumni\.alueducation\.com|si\.alueducation\.com)$",email):
            alu_emails.append(email)
     print("ALU emails are:", alu_emails)
+     card_pattern=r"\b(?:\d{4}[- ]?){3}\d{4}\b"
+    card=re.findall(card_pattern,text)
+    m_card=[]
+    for c in card:
+        good_card=re.sub(r"[- ]","",c)
+        mask=good_card[:4] + " **** **** " + good_card[-4:]
+        m_card.append(mask)
+    print("The credit card numbers are:", m_card)
